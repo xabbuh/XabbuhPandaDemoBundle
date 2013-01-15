89ffe8e733674e50a8b7226a64f258e512c01257
Christian Flothmann <christian.flothmann@xabbuh.de>
2013-01-15 20:47:59 +0100

    remove orphaned code

--- Controller/DemoController.php
+++ Controller/DemoController.php
@@ -83,40 +83,4 @@ class DemoController extends Controller
         $key = $this->container->get("xabbuh_panda_demo.twig.cloud_extension")->getSelectedCloud();
         return $this->container->get("xabbuh_panda.cloud_manager")->getCloud($key);
     }
-
-    public function plainVideosAction()
-    {
-        $apiHost = "api-eu.pandastream.com";
-        $path = "/videos.json";
-        $accessKey = "927e30d33a55eb373a99";
-        $secretKey = "13fa33b516893c1dcf6f";
-        $cloudId = "dd0997b5952a0d00d3da993b927171b5";
-        date_default_timezone_set("UTC");
-        $params = array(
-            "access_key" => $accessKey,
-            "timestamp" => date("c"),
-            "cloud_id" => $cloudId
-        );
-
-        $canonicalQueryString = str_replace(
-            array("+", "%5B", "%5D"),
-            array("%20", "[", "]"),
-            http_build_query($params)
-        );
-        $stringToSign = "GET\napi-eu.pandastream.com\n/videos.json\n" . $canonicalQueryString;
-        $params["signature"] = base64_encode(hash_hmac("sha256", $stringToSign, $secretKey, true));
-
-        $ch = curl_init("api-eu.pandastream.com/v2/videos.json?" . http_build_query($params));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        $response = curl_exec($ch);
-
-        if (curl_errno($ch) > 0) {
-            // error handling
-        }
-        return $this->render(
-            "XabbuhPandaDemoBundle:Demo:videos_plain.html.twig",
-            array("text" => $response)
-        );
-    }
 }
