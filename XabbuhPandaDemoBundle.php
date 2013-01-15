879ac44cd12ba02ee6da0deafcd941283b409506
Christian Flothmann <christian.flothmann@xabbuh.de>
2013-01-15 12:59:26 +0100

    initialise the bundle

---
+++ XabbuhPandaDemoBundle.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+* This file is part of the XabbuhPandaDemoBundle package.
+*
+* (c) Christian Flothmann <christian.flothmann@xabbuh.de>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Xabbuh\PandaDemoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * XabbuhPandaDemoBundle serves as an example bundle for how to use the features
+ * of the {@link https://github.com/xabbuh/XabbuhPandaBundle XabbuhPandaBundle}.
+ * 
+ * @author Christian Flothmann <christian.flothmann@xabbuh.de> 
+ */
+class XabbuhPandaDemoBundle extends Bundle
+{
+}
